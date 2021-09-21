2d78e9c1af1c4f1e4becc017f52637248a8ee6d8
Christopher Strickland <Christopher.T.Strickland-1@ou.edu>
2021-09-21 19:10:32 -0400

    small update to UI while rewatching videos

--- UI/MainMenu.cs
+++ UI/MainMenu.cs
@@ -3,7 +3,7 @@ using StoreBL;
 
 namespace UI
 {
-    public class MainMenu : IMenu //we implement IMenu which contains the start method for the user 
+     public class MainMenu : IMenu //we implement IMenu which contains the start method for the user 
     {
         //main menu needs ways to access BL
         private IBL _bl;
@@ -13,7 +13,41 @@ namespace UI
         }
         public void Start()
         {
-            Console.WriteLine("This is Main Menu");
+            bool exit = false; //bool variable to enter and exit the do-while loop 
+            string input = ""; //default string variable to take in user input   
+            do
+            {
+                Console.WriteLine("Here is the main menu:");
+                Console.WriteLine("[0] Explore Store");
+                Console.WriteLine("[1] Leave Reviews");
+                Console.WriteLine("[x] Exit");
+
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+
+                    case "0":
+                        Console.WriteLine("You want to explore the store"); 
+                        break;
+
+                    case "1":
+                        Console.WriteLine("You want to leave a review");
+                        break;
+
+                    case "x":
+                        Console.WriteLine("Adios");
+                        exit = true; // when i know they want to leave. Flip exit to true. 
+                        break;
+
+                    default: //triggered when none of this matches
+                        Console.WriteLine("What are you doing???");
+                        break;
+                }
+            
+            }while (!exit); //when true, it will exit. 
+
         }
+
     }
 }
--- UI/Program.cs
+++ UI/Program.cs
@@ -9,9 +9,11 @@ namespace UI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to My Store!");
+            Console.WriteLine("Welcome to Lucky Disks!");
             new MainMenu(new BL(new ExampleRepo())).Start(); //main menu needs a instance of business logic 
-            //business logic needs something of an instance that implements IRepo
+            //b
+            
+
         }
     }
 }
