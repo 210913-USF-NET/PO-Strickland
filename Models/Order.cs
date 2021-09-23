82b9bb8edac997f52da62711088082d2298ccd72
Christopher Strickland <Christopher.T.Strickland-1@ou.edu>
2021-09-22 22:54:58 -0400

    need to look at again

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,10 +1,22 @@
+using System.Net.Security;
 using System.Collections.Generic;
 namespace Models
 {
     public class Order //Customers owns Orders 
     {
+        // public Order(decimal total) : this(){
+        //     this.Total = total;
+        // }
+
+        // public Order(){
+        //     this.LineItem = new List<LineItem>();
+        // }
         public List<LineItem> LineItems { get; set; }
         public decimal Total { get; set; }
+
+        // public override string ToString(){
+        //     return $"Total Price: {this.Total}\n";
+        // }
 
     }
 }
