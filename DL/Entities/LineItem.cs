0be6102aa95d53e17168336438db4ed4cd42080b
Christopher Strickland <Christopher.T.Strickland-1@ou.edu>
2021-10-05 19:29:24 -0400

    update

--- DL/Entities/LineItem.cs
+++ DL/Entities/LineItem.cs
@@ -8,8 +8,14 @@ namespace DL.Entities
     public partial class LineItem
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int Quantity { get; set; }
-        public string Email { get; set; }
+        public int? ProductId { get; set; }
+        public int? OrderId { get; set; }
+        public int? StoreId { get; set; }
+        public int? ItemQuantity { get; set; }
+
+        public virtual Order Order { get; set; }
+        public virtual Product Product { get; set; }
+        public virtual StoreFront Store { get; set; }
     }
 }
+
