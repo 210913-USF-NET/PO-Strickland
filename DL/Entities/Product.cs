c8248b5f4184075fcb856a0f1b3967d76693c744
Christopher Strickland <Christopher.T.Strickland-1@ou.edu>
2021-10-05 19:31:11 -0400

    update

--- DL/Entities/Product.cs
+++ DL/Entities/Product.cs
@@ -7,10 +7,21 @@ namespace DL.Entities
 {
     public partial class Product
     {
+        public Product()
+        {
+            Inventories = new HashSet<Inventory>();
+            LineItems = new HashSet<LineItem>();
+            Orders = new HashSet<Order>();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Price { get; set; }
+        public int? Price { get; set; }
         public string Genre { get; set; }
-        public int Quantity { get; set; }
+        public int? Quantity { get; set; }
+
+        public virtual ICollection<Inventory> Inventories { get; set; }
+        public virtual ICollection<LineItem> LineItems { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
     }
 }
