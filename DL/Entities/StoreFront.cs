2cc0e49baf2be03c77c36b531042d401d715b80a
Christopher Strickland <Christopher.T.Strickland-1@ou.edu>
2021-10-05 19:32:49 -0400

    update

--- DL/Entities/StoreFront.cs
+++ DL/Entities/StoreFront.cs
@@ -7,8 +7,19 @@ namespace DL.Entities
 {
     public partial class StoreFront
     {
+        public StoreFront()
+        {
+            Inventories = new HashSet<Inventory>();
+            LineItems = new HashSet<LineItem>();
+            Orders = new HashSet<Order>();
+        }
+
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string StoreFrontName { get; set; }
         public string Address { get; set; }
+
+        public virtual ICollection<Inventory> Inventories { get; set; }
+        public virtual ICollection<LineItem> LineItems { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
     }
 }
