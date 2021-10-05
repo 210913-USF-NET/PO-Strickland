b9b2b280e08fe26b5a350ac3e7d751382dd2ad63
Christopher Strickland <Christopher.T.Strickland-1@ou.edu>
2021-10-05 19:01:00 -0400

    Update

--- UI/Product.cs
+++ UI/Product.cs
@@ -16,9 +16,17 @@ namespace UI
             _bl = bl;
 
         }
+        // private readonly Random _random = new Random();  
+  
+        // // Generates a random number within a range.      
+        // public int RandomNumber(int min, int max)  
+        // {  
+        //     return _random.Next(min, max);  
+        // }
 
         public void Start()
         {   
+                // int id = _random.Next(100, 1000000);
                 //int totalQuantity;
                 Console.WriteLine("Products at Lucky Disks\n");
                 Console.WriteLine("Name: ");
@@ -31,14 +39,14 @@ namespace UI
                 Console.WriteLine("Genre: ");
                 string genre = Console.ReadLine();
                 
-                Console.WriteLine("Quantity: ");
-                string quantity = Console.ReadLine();
-                int parsedQuantity;
-                bool parseSuccess = int.TryParse(quantity, out parsedQuantity);
+                // Console.WriteLine("Quantity: ");
+                // string quantity = Console.ReadLine();
+                // int parsedQuantity;
+                // bool parseSuccess = int.TryParse(quantity, out parsedQuantity);
 
                 
                 //Product newProduct = new Product(name, price, genre, totalQuantity);
-                Product newProduct = new Product(name, parsedPrice, genre, parsedQuantity); //inheritance
+                Product newProduct = new Product(name, parsedPrice, genre); //inheritance
                 AddProduct(newProduct); //this add the new product to the AddProduct constructor which takes it to the BL layer
                 Console.WriteLine($"\n\nThis product has been updated as: {newProduct.ToString()}");
 
