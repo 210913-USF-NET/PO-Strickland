d3b13818981cefac1708e07fe026545a61049a97
Christopher Strickland <Christopher.T.Strickland-1@ou.edu>
2021-10-05 19:34:22 -0400

    update

--- DL/Entities/Order.cs
+++ DL/Entities/Order.cs
@@ -1,12 +1,26 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+#nullable disable
 
 namespace DL.Entities
 {
-    public class Order
+    public partial class Order
     {
-        
+        public Order()
+        {
+            LineItems = new HashSet<LineItem>();
+        }
+
+        public int Id { get; set; }
+        public int? CustomersId { get; set; }
+        public int? ProductId { get; set; }
+        public int? StoreFrontId { get; set; }
+        public int? TotalAmount { get; set; }
+
+        public virtual Customer Customers { get; set; }
+        public virtual Product Product { get; set; }
+        public virtual StoreFront StoreFront { get; set; }
+        public virtual ICollection<LineItem> LineItems { get; set; }
     }
 }
