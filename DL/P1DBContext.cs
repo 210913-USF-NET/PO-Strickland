794f1b7b0212f08384d8514de79453249d9254c2
Christopher Strickland <Christopher.T.Strickland-1@ou.edu>
2021-10-05 21:47:13 -0400

    bringing in Web UI

---
+++ DL/P1DBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DL
+{
+    public class P1DBContext : DbContext
+    {
+        public P1DBContext() : base() { }
+        public P1DBContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<LineItem> LineItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<StoreFront> StoreFronts { get; set; }
+    }
+}
